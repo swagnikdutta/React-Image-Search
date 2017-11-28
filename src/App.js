e7d2ef44c077d8da680b2dedfbbd4e798f0d9694
Swagnik Dutta <swagnikd@gmail.com>
2017-11-28 12:08:24 +0530

    Error Handling done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './styles/App.css';
 
 // components
@@ -34,10 +35,10 @@ class App extends Component{
 	makeAPICall(){
 
 		// not mine
-		// var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyD6cZJdS4fjnrRzvM-WlEBeXKCEaBN9bbc&cx=017725526660795934517:_kokdupgki4';
+		var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyD6cZJdS4fjnrRzvM-WlEBeXKCEaBN9bbc&cx=017725526660795934517:_kokdupgki4';
 
 		// mine
-		var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyC3ztOjElYBVj8q2BSmIKJ4EdXVtDq6R18&cx=000966039869815770295:1ugxnp2e6nw';
+		// var url = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyC3ztOjElYBVj8q2BSmIKJ4EdXVtDq6R18&cx=000966039869815770295:1ugxnp2e6nw';
 
 		axios.get(url, {
 			params: {
@@ -57,7 +58,57 @@ class App extends Component{
 
 		})
 		.catch((e)=>{
-			console.log(e);
+
+			var errorCode = e.response.status;
+			var errorMessage = e.response.data.error.message;
+			var responseUrl = e.response.request.responseURL;
+			const errorStyle = {
+				'textAlign' : 'center',
+				'marginTop': '50px'
+			}
+
+			if(errorCode === 400){
+				
+				const element = (<div style={errorStyle}>
+									<h1>{errorCode} Bad Request</h1>
+									<h3>{errorMessage}</h3>
+									<p><b>ResponseUrl:</b> {responseUrl}</p>
+									<hr/>
+									<p>Tip: This generally happens when value of start parameter reaches 100 or more</p>
+								</div>);
+
+				ReactDOM.render(
+					element,
+					document.getElementById('root')
+				)
+			}
+			else if(errorCode === 403){
+				
+				const element = (<div style={errorStyle}>
+									<h1>{errorCode} Daily Usage Limits Expired</h1>
+									<h4>{errorMessage}</h4>
+									<p><b>ResponseUrl:</b> {responseUrl}</p>
+									<hr/>
+									<p>Tip: Maximum of 100 requests are allowed per day.</p>
+								</div>);
+
+				ReactDOM.render(
+					element,
+					document.getElementById('root')
+				)
+
+			} else{
+
+				console.log(e);
+				const element = (<div style={errorStyle}>
+									<h2>Something went wrong. Check console to see the error.</h2>
+								</div>);
+
+				ReactDOM.render(
+					element,
+					document.getElementById('root')
+				)
+			}
 		});
 	}
 
